36efe9790c920100e311b6f0bbb8650a3f5293b4
Giovannasoares0310 <giovannasoares031008@gmail.com>
2025-03-27 20:51:19 -0300

    subindo para o repositorio

---
+++ Exercícios/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercícios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] vetor = new int[15];
+            int i;
+            Random Num = new Random();
+
+            for (i = 0; i < 15; i++) 
+            {
+                vetor[i] = Num.Next(0,50);
+            }
+
+            for (i = 0; i < 15; i++)
+            {
+                if (vetor[i] % 2 == 0)
+                {
+                    rtxt1.Text += vetor[i].ToString() + Environment.NewLine;
+                }
+            }
+        }
+    }
+}
